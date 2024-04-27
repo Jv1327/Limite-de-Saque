cbdd3de7b052d79981b69f6d23c50e3b51ba75ee
Jv1327 <jvrodrigues132770@gmail.com>
2024-04-27 19:32:25 -0300

    first commit

---
+++ VerificacaoChequeEspecial.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class VerificacaoChequeEspecial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = scanner.nextDouble();
+        double saque = scanner.nextDouble();
+        double limiteChequeEspecial = 500;
+
+        // Verifique se o saque não ultrapassa o saldo disponível na conta:
+        if (saque <= saldo) {
+            // Transacao realizada com sucesso
+            System.out.println("Transacao realizada com sucesso.");
+        } else {
+            // Verifique se o saque ultrapassa o limite do cheque especial, mas não o saldo total disponível:
+            if (saque <= saldo + limiteChequeEspecial) {
+                // Transacao realizada com sucesso utilizando o cheque especial
+                System.out.println("Transacao realizada com sucesso utilizando o cheque especial.");
+            } else {
+                // Se o saque ultrapassar tanto o saldo disponível quanto o limite do cheque especial
+                System.out.println("Transacao nao realizada. Limite do cheque especial excedido.");
+            }
+        }
+
+        // Fechamos o objeto Scanner para liberar os recursos:
+        scanner.close();
+    }
+}
